f7c0e6902897688cf618ba17fb540bf4bc0f60ad
shonmikl <mikhailegorov676@gmail.com>
2023-03-07 14:20:13 +0300

    Paskal.java

---
+++ src/_2023_03_07/Eratosfen.java
@@ -0,0 +1,67 @@
+package _2023_03_07;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Eratosfen {
+    public static void main(String[] args) {
+        //System.out.println(eratosfen(122));
+        //System.out.println(isPrime(7));
+        System.out.println(isPrimeOpt(43));
+    }
+
+    public static List<Integer> eratosfen(int max) {
+        boolean[] isPrime = new boolean[max];
+        Arrays.fill(isPrime, true);
+
+        for (int i = 2; (i * i) < max; i++) {
+            if (isPrime[i]) {
+                for (int j = (2 * i); j < max; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        List<Integer> primes = new ArrayList<>();
+        for (int i = 2; i < max; i++) {
+            if (isPrime[i]) {
+                primes.add(i);
+            }
+        }
+        return primes;
+    }
+
+    private static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; (i * i) <= number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPrimeOpt(int number) {
+        if (number < 2) {
+            return false;
+        }
+
+        if (number % 2 == 0) {
+            return number == 2;
+        }
+
+        if (number % 3 == 0) {
+            return number == 3;
+        }
+
+        for (int i = 5; (i * i) <= number; i += 6) {
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/_2023_03_07/paskal/Paskal.java
@@ -0,0 +1,10 @@
+package _2023_03_07.paskal;
+
+import java.util.List;
+
+//todo
+public class Paskal {
+    private List<List<Integer>> paskal(int numRows) {
+
+    }
+}
