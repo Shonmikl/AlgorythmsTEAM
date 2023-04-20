1d745beaf18c5e1ef7f8ae1f1791dad3ee263d93
shonmikl <mikhailegorov676@gmail.com>
2023-04-20 12:44:53 +0200

    queen

--- src/_2023_04_06/Task05.java
+++ src/_2023_04_06/Task05.java
@@ -1,5 +1,8 @@
 package _2023_04_06;
 
+import lombok.ToString;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.TreeMap;
@@ -115,20 +118,6 @@ public class Task05 {
     }
 
     /**
-     * 1 -> 2 -> 3
-     * 4 -> 5 -> 6
-     * output 1 - 2 - 3 - 4 - 5 - 6
-     * <p>
-     * 1 -> 2 - > 7
-     * 3 -> 3 -> 11
-     * output 1 - 2 - 3 - 3 - 7 - 11
-     */
-
-    public static ListNodes1 mergeTwo(ListNodes1 l1, ListNodes1 l2) {
-        return null;
-    }
-
-    /**
      * int[] arr = [-1 0 -9 8 11 -3 1 5]
      * int target = X
      * <p>
@@ -146,37 +135,145 @@ public class Task05 {
             int b = nums.length - 1;
             while (a < b) {
                 int currentSum = nums[i] + nums[a] + nums[b];
-                if (currentSum > target) {
-                    b -= 1;
-                } else {
-                    a += 1;
+                if (currentSum == target) {
+                    return target;
                 }
+
                 if (Math.abs(currentSum - target) < Math.abs(result - target)) {
                     result = currentSum;
+                }
+
+                if (currentSum > target) {
+                    b--;
+                } else a++;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * int[] king = {1, 1}
+     * int[][] queen = {[2,3], [6,2], [5,5]}
+     */
+
+    private static List<List<Integer>> queens(int[][] queens, int[] king) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[][] seen = new boolean[10000][10000];
+        for (int[] q : queens) {
+            seen[q[0]][q[1]] = true;
+        }
+
+        int[] direction = {-1, 0, 1};
+        for (int dx : direction) {
+            for (int dy : direction) {
+                if (dx == 0 && dy == 0) {
+                    continue;
+                }
+
+                int x = king[0];
+                int y = king[1];
+
+                while (x + dx >= 0 && x + dx < 10000 && y + dy >= 0 && y + dy < 10000) {
+                    x += dx;
+                    y += dy;
+
+                    if (seen[x][y]) {
+                        result.add(Arrays.asList(x, y));
+                        break;
+                    }
                 }
             }
         }
         return result;
     }
 
+    private static List<Integer> queenOlgaMira(int[][] queen, int[] king) {
+        List<Integer> res = new ArrayList<>();
+        int x = king[0];
+        int y = king[1];
+        for (int i = 0; i < queen.length; i++) {
+            if (x == queen[i][0]) {
+                res.add(i);
+            } else if (y == queen[i][1]) {
+                res.add(i);
+            } else if ((queen[i][0] - x) == (queen[i][1] - y)) {
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+
 
     public static void main(String[] args) {
-        int[] arr = {0, 1, 2, 3, 4, 5, 6};
-        int target = 2;
-        System.out.println(threeSum(arr, target));
+//        int[] arr = {0, 1, 2, 3, 4, 5, 6};
+//        int target = 2;
+//        System.out.println(threeSum(arr, target));
+//
+//
+//        // toLowerCaseLera("TYUuuyRE");
+//        //System.out.println(getHand(new int[]{1, 2, 3, 6, 2, 3, 4, 7, 9}, 3));
+//        System.out.println(runAway("UL"));
 
+        int[] king = {1, 2};
+        int[][] queen = {{2, 3}, {1, 5}, {1, 1}, {4, 2}, {5, 5}, {2, 7}, {3, 7}, {4, 7}, {3, 5}};
+        int[][] queenTEST = {{1998, 3},  {1, 9997}, {9988, 9989}};
+        System.out.println(queenOlgaMira(queenTEST, king));
+        System.out.println(queens(queenTEST, king));
 
-        // toLowerCaseLera("TYUuuyRE");
-        //System.out.println(getHand(new int[]{1, 2, 3, 6, 2, 3, 4, 7, 9}, 3));
-        System.out.println(runAway("UL"));
     }
 }
 
-class ListNodes1 {
+@ToString
+class ListNodes {
     int val;
-    ListNodes1 next;
+    ListNodes next;
 
-    public ListNodes1(int val) {
+    public ListNodes(int val) {
         this.val = val;
+    }
+
+    public static void main(String[] args) {
+        ListNodes l1 = new ListNodes(1);
+        l1.next = new ListNodes(3);
+
+        ListNodes l2 = new ListNodes(2);
+
+        ListNodes res = mergeTwo(l1, l2);
+        System.out.println(res);
+    }
+
+    /**
+     * 1 -> 2 -> 3
+     * 4 -> 5 -> 6
+     * output 1 - 2 - 3 - 4 - 5 - 6
+     * <p>
+     * 1 -> 2 - > 7
+     * 3 -> 3 -> 11
+     * output 1 - 2 - 3 - 3 - 7 - 11
+     */
+
+    public static ListNodes mergeTwo(ListNodes l1, ListNodes l2) {
+        ListNodes tempNode = new ListNodes(0);
+        ListNodes currentNode = tempNode;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                currentNode.next = l1;
+                l1 = l1.next;
+            } else {
+                currentNode.next = l2;
+                l2 = l2.next;
+            }
+            currentNode = currentNode.next;
+        }
+        if (l1 != null) {
+            currentNode.next = l1;
+        }
+
+        if (l2 != null) {
+            currentNode.next = l2;
+        }
+        return tempNode.next;
     }
 }
